71412897502a94886286871244852df422dafbbf
gela pipia <gelapipia10@gmail.com>
2023-12-07 00:28:50 +0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import CostumerReviews from "./sections/CostumerReviews"
+import Footer from "./sections/Footer"
+import Hero from "./sections/Hero"
+import Offer from "./sections/Offer"
+import Products from "./sections/Products"
+import Quality from "./sections/Quality"
+import Services from "./sections/Services"
+import Subscribe from "./sections/Subscribe"
+import Navbar from "./components/Navbar"
+
+const App = () => {
+  return (
+    <main className="relative">
+      <Navbar />
+      <section className="xl:padding-1 wide:padding-r padding-b">
+        <Hero />
+      </section>
+      <section className="padding">
+        <Products />
+      </section>
+      <section className="padding">
+        <Quality />
+      </section>
+      <section className="padding-x">
+        <Services />
+      </section>
+      <section className="padding">
+        <Offer />
+      </section>
+      <section className="padding bg-pale-blue">
+        <CostumerReviews />
+      </section>
+      <section className="padding-x sm:py-32 py-16">
+        <Subscribe />
+      </section>
+      <section className="bg-black padding-x padding-t pb-8">
+        <Footer />
+      </section>
+    </main>
+  )
+}
+
+export default App;
