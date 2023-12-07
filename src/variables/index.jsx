16de2f0c3b8678256817d30321c65dfb74120b0d
gela pipia <gelapipia10@gmail.com>
2023-12-07 23:21:09 +0400

    updated variables file: added products array of objects

--- src/variables/index.jsx
+++ src/variables/index.jsx
@@ -1,6 +1,6 @@
-import { bigShoe1, bigShoe2, bigShoe3, thumbnailShoe1, thumbnailShoe2, thumbnailShoe3 } from "../assets/images";
+import { bigShoe1, bigShoe2, bigShoe3, thumbnailShoe1, thumbnailShoe2, thumbnailShoe3, shoe4, shoe5, shoe6, shoe7 } from "../assets/images";
 
-
+// For navigation menu
 export const navLinks = [
     { href: "#home", label: "Home" },
     { href: "#about-us", label: "About Us" },
@@ -8,7 +8,7 @@ export const navLinks = [
     { href: "#contact-us", label: "Contact Us" },
 ];
 
-
+// For Hero section images
 export const shoes = [
     {
         thumbnail: thumbnailShoe1,
@@ -24,10 +24,39 @@ export const shoes = [
     },
 ];
 
-
+// For Hero section Stataistics
 export const statistics = [
     { value: '1k+', label: 'Brands' },
     { value: '500+', label: 'Shops' },
     { value: '250k+', label: 'Customers' },
+];
+
+
+//For products section
+export const products = [
+    {
+        imgURL: shoe4,
+        name: "Nike Air Jordan-01",
+        price: "$200.20",
+        rating: 4.5,
+    },
+    {
+        imgURL: shoe5,
+        name: "Nike Air Jordan-10",
+        price: "$210.20",
+        rating: 5.0,
+    },
+    {
+        imgURL: shoe6,
+        name: "Nike Air Jordan-100",
+        price: "$220.20",
+        rating: 4.8,
+    },
+    {
+        imgURL: shoe7,
+        name: "Nike Air Jordan-001",
+        price: "$230.20",
+        rating: 4.4,
+    },
 ];
 
