0649e3a464642f31731ed4d628a17a8d2e8d740f
gela pipia <gelapipia10@gmail.com>
2023-12-08 22:31:36 +0400

    filled CustomerReview section with content

--- src/sections/CostumerReviews.jsx
+++ src/sections/CostumerReviews.jsx
@@ -1,7 +1,27 @@
+import { reviews } from "../variables";
+import ReviewCardComponent from "../components/ReviewCardComponent";
+
 const CostumerReviews = () => {
   return (
-    <div>CostumerReviews</div>
-  )
-}
+    <section className="max-container">
+      <h3 className="font-palanquin text-4xl text-center font-bold">
+        What Our
+        <span className="text-coral-red"> Costumers </span>
+        Are Saying?
+      </h3>
+      <p className="text-center text-slate-gray font-normal font-montserrat mt-3">
+        We take immense pride in curating an unparalleled shopping experience,
+        and our customers' <br /> feedback speaks volumes about their journey with us
+      </p>
 
-export default CostumerReviews
+      <div className="mt-20 flex flex-1 justify-evenly items-center max-lg:flex-col gap-12">
+        {reviews.map(review => (
+          <ReviewCardComponent key={review.customerName} review={review} />
+        ))}
+      </div>
+      
+    </section>
+  );
+};
+
+export default CostumerReviews;
