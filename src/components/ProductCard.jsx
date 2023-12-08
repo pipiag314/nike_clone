37489f9f9331894214ef04e7d40e816a061992ab
gela pipia <gelapipia10@gmail.com>
2023-12-08 16:05:36 +0400

    modified ProductCard components styles

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -16,13 +16,13 @@ const ProductCard = ({ product }) => {
         "
         src={product.imgURL}
       />
-      <div className="mt-6 gap-1 flex justify-between px-4 font-bold">
-        <p className="flex gap-2">
+      <div className="mt-3 gap-1 flex flex-col px-4 font-bold">
+        <p className="flex gap-2 text-slate-gray">
             <img src={star} width={24} height={24} />
             {product.rating}
         </p>
-        <h1>{product.name}</h1>
-        <p>{product.price}</p>
+        <h2 className="text-xl leading-normal font-medium font-palanquin">{product.name}</h2>
+        <p className="font-bold font-montserrat text-coral-red">{product.price}</p>
       </div>
     </div>
   );
