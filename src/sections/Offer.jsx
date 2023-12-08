a37f82cb267c6ad806a7482dd29cb99ca16e509e
gela pipia <gelapipia10@gmail.com>
2023-12-08 22:32:54 +0400

    Offer and Subscription sections

--- src/sections/Offer.jsx
+++ src/sections/Offer.jsx
@@ -1,7 +1,43 @@
+import Button from "../components/Button";
+import { offer } from "../assets/images/index";
+import { arrowRight } from "../assets/icons/index";
+
 const Offer = () => {
   return (
-    <div>Offer</div>
-  )
-}
+    <section className="flex items-center max-xl:flex-col-reverse max-xl:gap-10 max-container">
+      <div className="">
+        <img src={offer} width={773} height={687} className="object-contain" />
+      </div>
+      <div className="flex flex-1 flex-col">
+        <h2 className="xl:text-8xl text-4xl font-bold font-palanquin">
+          <span className="text-coral-red">Special </span>
+          Offer
+        </h2>
+        <p className="font-montserrat font-normal text-slate-gray mt-5">
+          Ready to elevate your shoe game? Dive into the ultimate footwear
+          experience with [Your Shoe Store Name]! For a limited time, we're
+          delighted to offer you an exclusive 30% discount on our entire range
+          of stylish shoes
+          <br />
+          <br />
+          Walk with confidence in our trendsetting collection that seamlessly
+          blends style and comfort. From casual kicks to statement heels, we
+          have the perfect pair for every occasion.
+        </p>
+        <div className="w-full flex justify-start px-10 gap-5">
+          <Button
+            className="mt-5 py-5 hover:text-black transition duration-200"
+            content="View Details"
+            icon={arrowRight}
+          />
+          <Button
+            className="mt-5 py-5 px-7 bg-white text-coral-red hover:text-black transition duration-200"
+            content="Learn More"
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Offer
+export default Offer;
--- src/sections/Subscribe.jsx
+++ src/sections/Subscribe.jsx
@@ -1,7 +1,27 @@
+import Button from "../components/Button";
+
 const Subscribe = () => {
   return (
-    <div>Subscribe</div>
-  )
-}
+    <section
+      id="contact-us"
+      className="
+        max-container
+        flex
+        justify-center
+        items-center
+        max-lg:flex-col
+        gap-10
+      ">
+      <h2 className="text-xl lg:max-w-md font-bold font-palanquin">
+        Sign Up for
+        <span className="text-coral-red"> Updates </span>& Newsletter
+      </h2>
+      <div className="flex max-sm:flex-col max-sm:gap-5 items-center w-full lg:max-w-[40%] sm:border sm:border-slate-gray p-1 rounded-full">
+        <input type="text" placeholder="Subscribe@nike.com" className="input" />
+        <Button content="Subscribe" className="max-sm:w-full" />
+      </div>
+    </section>
+  );
+};
 
-export default Subscribe
+export default Subscribe;
