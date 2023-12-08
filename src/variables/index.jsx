c133379db8bab1310c453127f9c467884dc54c6a
gela pipia <gelapipia10@gmail.com>
2023-12-08 22:33:41 +0400

    added data for CustomerService and Footer sections

--- src/variables/index.jsx
+++ src/variables/index.jsx
@@ -1,5 +1,5 @@
-import { bigShoe1, bigShoe2, bigShoe3, thumbnailShoe1, thumbnailShoe2, thumbnailShoe3, shoe4, shoe5, shoe6, shoe7 } from "../assets/images";
-import { truckFast, shieldTick, support } from "../assets/icons/index"
+import { customer1, customer2, bigShoe1, bigShoe2, bigShoe3, thumbnailShoe1, thumbnailShoe2, thumbnailShoe3, shoe4, shoe5, shoe6, shoe7 } from "../assets/images";
+import { truckFast, shieldTick, support, facebook, twitter, instagram } from "../assets/icons/index"
 
 // For navigation menu
 export const navLinks = [
@@ -79,3 +79,58 @@ export const services = [
         subtext: "Our dedicated team is here to assist you every step of the way."
     },
 ];
+
+// FOR CostumerReviews section
+export const reviews = [
+    {
+        imgURL: customer1,
+        customerName: 'Morich Brown',
+        rating: "4.8",
+        feedback: "The attention to detail and the quality of the product exceeded my expectations. Highly recommended!"
+    },
+    {
+        imgURL: customer2,
+        customerName: 'Lota Mongeskar',
+        rating: "5.0",
+        feedback: "The product not only met but exceeded my expectations. I'll definitely be a returning customer!"
+    }
+]
+
+
+//FOR footer section
+export const footerLinks = [
+    {
+        title: "Products",
+        links: [
+            { name: "Air Force 1", link: "/" },
+            { name: "Air Max 1", link: "/" },
+            { name: "Air Jordan 1", link: "/" },
+            { name: "Air Force 2", link: "/" },
+            { name: "Nike Waffle Racer", link: "/" },
+            { name: "Nike Cortez", link: "/" },
+        ],
+    },
+    {
+        title: "Help",
+        links: [
+            { name: "About us", link: "/" },
+            { name: "FAQs", link: "/" },
+            { name: "How it works", link: "/" },
+            { name: "Privacy policy", link: "/" },
+            { name: "Payment policy", link: "/" },
+        ],
+    },
+    {
+        title: "Get in touch",
+        links: [
+            { name: "customer@nike.com", link: "mailto:customer@nike.com" },
+            { name: "+92554862354", link: "tel:+92554862354" },
+        ],
+    },
+]
+
+export const socialMedia = [
+    { src: facebook, alt: "facebook logo" },
+    { src: twitter, alt: "twitter logo" },
+    { src: instagram, alt: "instagram logo" },
+]
